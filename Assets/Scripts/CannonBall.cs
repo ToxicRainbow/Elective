251b8151d6bdc08b9a00a58d7f078e8c6efb4ac1
Toxic <marcelweetikveel@hotmail.nl>
2020-03-23 05:26:44 +0100

    bullets now die

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -7,7 +7,7 @@ public class CannonBall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +15,16 @@ public class CannonBall : MonoBehaviour
     {
         transform.Translate(new Vector3(0f, 0f, 500 * Time.fixedDeltaTime), Space.Self);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "GameWall")
+        {
+            Destroy(gameObject);
+        }
+
+        
+    }
+
 }
