bb2d6b79da851369060ecc4ae6352c0440c3db7d
Toxic <marcelweetikveel@hotmail.nl>
2020-06-03 19:51:53 +0200

    marcel AI

--- Assets/Scripts/MarcelAI.cs
+++ Assets/Scripts/MarcelAI.cs
@@ -6,14 +6,19 @@ public class MarcelAI : BaseAI
 {
     public override IEnumerator RunAI()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 20; i++)
         {
-            yield return Ahead(200);
+            yield return Ahead(50);
             yield return FireFront(1);
-            yield return TurnLookoutLeft(90);
+            //yield return TurnLookoutLeft(90);
             yield return TurnLeft(360);
-            yield return FireLeft(1);
-            yield return TurnLookoutRight(180);
+            //yield return FireLeft(1);
+            //yield return TurnLookoutRight(180);
         }
+    }
+
+    public override void OnScannedRobot(ScannedRobotEvent e)
+    {
+        Debug.Log("Ship detected: " + e.Name + " at distance: " + e.Distance);
     }
 }
