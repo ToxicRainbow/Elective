513c652291e8df5ad8f8f52493d29123cb647035
Toxic <marcelweetikveel@hotmail.nl>
2020-06-05 19:40:53 +0200

    cannonball spawnpoint adjustment

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -25,7 +25,7 @@ public class CannonBall : MonoBehaviour
         }
         if (other.tag == "Boat")
         {
-            //Destroy(gameObject);
+            Destroy(gameObject);
             Debug.Log("hit");
         }
     }
